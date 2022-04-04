382dd430c5fb8d066d89c306e1bcecd3ad2b0062
lucascr <lcrodrigues1995@gmail.com>
2022-04-04 19:14:39 -0300

    add event on menu option click

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@
 const hamburger = document.querySelector('.hamburger-btn');
 const dropdown = document.querySelector('.mobile-menu');
 const close = document.querySelector('.close-button');
+const closeList = document.querySelector('.mobile-menu-list');
 
 hamburger.addEventListener('click', function(){
     dropdown.classList.remove('hide');
@@ -10,3 +11,7 @@ hamburger.addEventListener('click', function(){
 close.addEventListener('click', function(){
   dropdown.classList.add('hide');
 }) 
+
+closeList.addEventListener('click', ()=>{
+  dropdown.classList.add('hide');
+})
