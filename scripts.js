1e9825fe803dab72be1e030ce891894b3272bd58
Gedewon <gedewon.hayle.9@gmail.com>
2022-04-05 18:56:37 +0300

    ADD mobile js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,48 @@
+const openMobileMenu = document.querySelectorAll('[data-mobile-menu-target]');
+const closeMobileMenu = document.querySelectorAll('[data-close-button]');
+const mainTag = document.querySelector('.main');
+const menuContainer = document.querySelector('.mobile-menu-list');
+const humburgerButton = document.querySelector('.hamburger-btn');
 
-const hamburger = document.querySelector('.hamburger-btn');
-const dropdown = document.querySelector('.mobile-menu');
-const close = document.querySelector('.close-button');
-const closeList = document.querySelector('.mobile-menu-list');
+const mobile_menu = document.querySelector('.mobile-menu')
 
-hamburger.addEventListener('click', function(){
-    dropdown.classList.remove('hide');
-}) 
+openMobileMenu.forEach(button => {
+    button.addEventListener('click', () => {
+        const mobileMenu = document.querySelector(button.dataset.mobileMenuTarget);
+        openMenu(mobileMenu, mainTag, humburgerButton);
+    })
+})
 
-close.addEventListener('click', function(){
-  dropdown.classList.add('hide');
-}) 
+closeMobileMenu.forEach(button => {
+    button.addEventListener('click', () => {
+        const mobileMenu = button.closest('.mobile-menu')
+        closeMenu(mobileMenu);
+    })
+})
 
-closeList.addEventListener('click', ()=>{
-  dropdown.classList.add('hide');
+function openMenu(mobileMenu, mainTag, humburgerButton) {
+    if(mobileMenu == null){
+        return
+    } else{
+
+        mobileMenu.classList.add('active');
+        mainTag.classList.add('fixed_postion');
+        humburgerButton.classList.add('hide');
+    }
+}
+
+function closeMenu(mobileMenu) {
+    if(mobileMenu == null) {
+        return
+    }else{
+        mainTag.classList.remove('fixed_postion')
+        mobileMenu.classList.remove('active');
+        humburgerButton.classList.remove('hide');
+    }
+}
+
+menuContainer.addEventListener('click', ()=> {
+    mainTag.classList.remove('fixed_postion');
+    mobile_menu.classList.remove('active');
+    humburgerButton.classList.remove('hide');
 })
