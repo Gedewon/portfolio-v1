596be8bede9d0eb8096e47adf801bc0b6d0b4372
Gedewon <gedewon.hayle.9@gmail.com>
2022-04-06 15:35:55 +0300

    Iterate to create element from js

--- scripts.js
+++ scripts.js
@@ -89,3 +89,57 @@ const projects = [
       name_detail: ["CANOPY", "Back End Dev", "2015"]
   }
 ]
+projectDetails.forEach((button)=>{
+  button.addEventListener('click', (e) => {
+      const selected_project = projects.filter((project)=> project.id == e.target.id)[0]
+      mainTag.classList.add('fixed_postion');
+      projectDetail.classList.add('active');
+      projectDetail.innerHTML = `
+      <article class="work-container flex project-detail-article project-flex">
+          <div class="project-detail-header">
+            <h3>${selected_project.name}</h3>
+            <button data-close-button class="close-button-project">&times;</button>
+         </div>
+         <div class="client-des">
+            <ul class="client-name">
+              <li>${selected_project.name_detail[0]}</li>
+            </ul>
+            <ul>
+              <li>${selected_project.name_detail[1]}</li>
+            </ul>
+            <ul>
+              <li>${selected_project.name_detail[2]}</li>
+            </ul>
+          </div>
+        <div class="work-img">
+          <img src=${selected_project.featured_image} alt="Picture for the project Tonic, showing a laptop and a widescreen." />
+        </div>
+        <div class="work-description-project project-detail-work-description">
+          <p>
+            ${selected_project.description}
+          </p>
+          <div class="project-detail-tech-btns">
+          <ul class="buttons project-detail-buttons">
+            <li><button type="button">${selected_project.technologies[0]}</button></li>
+            <li><button type="button">${selected_project.technologies[1]}</button></li>
+            <li><button type="button">${selected_project.technologies[2]}</button></li>
+          </ul>
+          <div class="project-detail-buttons">
+              <a href="https://Gedewon.github.io/Portfolio/"><button class="see-project-btn see-source">${selected_project.link_to_live[0]}
+                <img class="see-source-img" src=${selected_project.link_to_live[1]} alt="" /></button>
+              </a>
+              <a href="https://github.com/Gedewon/Portfolio"><button class="see-project-btn see-source">${selected_project.link_to_source[0]}
+                <img class="see-source-img" src=${selected_project.link_to_source[1]} alt="" /></button>
+              </a>
+          </div>
+          </div>
+        </div>
+      </article>`;
+      const projectDetailClose = document.querySelector('.close-button-project');
+      projectDetailClose.addEventListener('click', (e)=>{
+          mainTag.classList.remove('fixed_postion');
+          projectDetail.classList.remove('active');
+
+      })
+  })
+})
