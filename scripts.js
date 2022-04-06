663432a8345f7c20f0e62e94c2c686a89d9ffd58
Gedewon <gedewon.hayle.9@gmail.com>
2022-04-06 15:35:23 +0300

    create project as JS object

--- scripts.js
+++ scripts.js
@@ -42,3 +42,50 @@ menuContainer.addEventListener('click', () => {
   mobileMenu.classList.remove('active');
   humburgerButton.classList.remove('hide');
 });
+
+const projectDetails = document.querySelectorAll('.see-project-btn')
+const projectDetail = document.querySelector('.project-detail');
+
+
+const projects = [
+  {
+      id: 1,
+      name:  "Tonic",
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      featured_image: "./Images/work-one-img.jpg",
+      link_to_live: ["See live", "./Images/detailProjectLive.png"],
+      link_to_source: ["See source", "./Images/detailProjectGit.png"],
+      technologies: ["html", "css", "javaScript"],
+      name_detail: ["CANOPY", "Back End Dev", "2015"]
+  },
+  {
+      id: 2,
+      name:  "Multi-Post Stories",
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      featured_image: "./Images/work-two-img.jpg",
+      link_to_live: ["See live", "./Images/detailProjectLive.png"],
+      link_to_source: ["See source", "./Images/detailProjectGit.png"],
+      technologies: ["html", "css", "javaScript"],
+      name_detail: ["CANOPY", "Back End Dev", "2015"]
+  },
+  {
+      id: 3,
+      name:  "Tonic",
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      featured_image: "./Images/work-three-img.jpg",
+      link_to_live: ["See live", "./Images/detailProjectLive.png"],
+      link_to_source: ["See source", "./Images/detailProjectGit.png"],
+      technologies: ["html", "css", "javaScript"],
+      name_detail: ["CANOPY", "Back End Dev", "2015"]
+  },
+  {
+      id: 4,
+      name:  "Multi-Post Stories",
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      featured_image: "./Images/work-four-img.jpg",
+      link_to_live: ["See live", "./Images/detailProjectLive.png"],
+      link_to_source: ["See source", "./Images/detailProjectGit.png"],
+      technologies: ["html", "css", "javaScript"],
+      name_detail: ["CANOPY", "Back End Dev", "2015"]
+  }
+]
